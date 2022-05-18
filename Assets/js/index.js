adf1401d83c0224c9fdf02bd7fccc31254349c47
Maiyoj <valentinemaiyo1995@gmail.com>
2022-05-18 14:53:08 +0300

    add hover-color to javascript

---
+++ Assets/js/index.js
@@ -0,0 +1,10 @@
+
+let form = document.querySelector(".formfill")
+
+
+function  calendar(){
+
+    form.style.display = "anchored";
+    form.style.display = "block";
+    form.style.color = "red";
+}
