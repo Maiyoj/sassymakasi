152d8a59018c24790dd63d48a187f4e91eee18ab
gideonlangat98 <gideonlangat98@gmail.com>
2022-05-19 10:50:13 +0300

    added media query

---
+++ Assets/js/index.js
@@ -0,0 +1,14 @@
+
+// let but = document.getElementById("butown");
+
+// function myFunction() {
+//  but.innerHTML = "Read More";
+//  alert("but");
+
+// }
+
+let hist = document.getElementById("button");
+function myButton() {
+    hist.innerHTML= "View More";
+    alert("View more Pictures")
+}
