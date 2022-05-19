e524e2a146cf434770f8e831325b6127efcbd7d1
Maiyoj <valentinemaiyo1995@gmail.com>
2022-05-19 09:51:42 +0300

    add userfillin form

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,5 +1,6 @@
 
-let form = document.querySelector(".formfill")
+let form = document.querySelector(".formfill");
+let userDetails = document.querySelector(".form");
 function  calendar(){
     form.style.display = "block";
    
@@ -7,5 +8,8 @@ function  calendar(){
 
 function  out(){
     form.style.display = "none";
-   
+}
+
+function book(){
+
 }
